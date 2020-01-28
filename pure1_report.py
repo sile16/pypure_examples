828553d75f649b3d4ce4fd112707bb5e96dcfd5b
Matt Robertson <sile16@gmail.com>
2020-01-27 19:12:52 -0600

    added a progress bar, and pausing to avoid  hitting API limits.

--- pure1_report.py
+++ pure1_report.py
@@ -1,8 +1,30 @@
 #! /usr/bin/env python3
 
+# The MIT License (MIT)
+# Copyright (c) 2016 Vladimir Ignatev
+#
+# Permission is hereby granted, free of charge, to any person obtaining 
+# a copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the Software 
+# is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import argparse
 import datetime
 import csv
+import sys
+import time
 
 from pypureclient import pure1
 
@@ -13,14 +35,23 @@ BYTES_IN_A_GIGABYTE = 1073741824
 queries_count = 1
 sorted_metrics = None
 
+# progress bar.
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
 def generate_fleet_report(pure1_api_id, pure1_pk_file, pure1_pk_pwd):
 
     pure1Client = pure1.Client(private_key_file=pure1_pk_file,
                                private_key_password=pure1_pk_pwd, app_id=pure1_api_id)
 
-    # Get all Flash Arrays.
-    # response = pure1Client.get_arrays(filter="contains(model,'FB')")
+    # Get all  Arrays, FlashArray & FlashBlade.
     response = pure1Client.get_arrays()
 
     arrays = []
@@ -44,32 +75,69 @@ def generate_fleet_report(pure1_api_id, pure1_pk_file, pure1_pk_pwd):
                                 'File System Space (TB)', 'Object Store Space (TB)'])
 
             # Go through all arrays
+            count = 0
+            total = len(arrays)
             for array in arrays:
-                os_version = str.format("{} {}", array.os, array.version)
                 if 'FA' in array.os:
+                    # FlashArray Metrics
                     metrics_names = ['array_total_capacity', 'array_data_reduction', 'array_shared_space', 
-                                    'array_volume_space', 'array_snapshot_space', 'array_system_space', 'array_total_load']
-                    if 'CBS' in array.model:
-                        # Only our Capacity on demand & CBS have effective_used_space
-                        metrics_names.append('array_effective_used_space')
+                                    'array_volume_space', 'array_snapshot_space', 'array_system_space', 
+                                    'array_total_load']
                 else:
+                    # FlashBlade Metrics
                     metrics_names = ['array_total_capacity', 'array_data_reduction', 
                                     'array_file_system_space', 'array_object_store_space' ]
-                start = int((datetime.datetime.now(
-                ) - datetime.timedelta(days=REPORTING_INTERVAL_DAYS)).timestamp())
+                
+                # We are required to specify a start and end date
+                # so just grabbing a couple days and will use the last day value returned.
+                start = int((datetime.datetime.now() - datetime.timedelta(days=REPORTING_INTERVAL_DAYS)).timestamp())
                 end = int((datetime.datetime.now()).timestamp())
 
                 # Request the last 7 days of data, each day metric summarized by the max value.
+                # for this particular array.
                 response = pure1Client.get_metrics_history(aggregation='max', names=metrics_names, resource_ids=array.id,
                                                         resolution=METRIC_RESOLUTION_DAY, start_time=start, end_time=end)
-
+                
+                # Check to see if we should delay so we don't hit an API limit
+                # add some delays in here  if we get too close to limit.
+                
+                progress(count, len(arrays), 'Getting array metrics.                           ')
+                if int(response.headers.x_ratelimit_remaining_second) == 1:
+                    #progress(count, len(arrays), "Sleeping because of API per 5 sec rate limit")
+                    time.sleep(0.5)
+                if int(response.headers.x_ratelimit_remaining_second) == 0:
+                    progress(count, len(arrays), "Sleeping because of API per 5 sec rate limit")
+                    time.sleep(2)
+                if int(response.headers.x_ratelimit_remaining_minute) < 15:
+                    progress(count, len(arrays), "Sleeping because of API per minute rate limit")
+                    time.sleep(2)
+                if int(response.headers.x_ratelimit_remaining_minute) < 5:
+                    progress(count, len(arrays), "Sleeping because of API per minute rate limit")
+                    time.sleep(5)
+                    
+                # Increase the count for the progress bar
+                count += 1
+                
+                # we want to null all values, so we don't accidenlty return
+                # a value from the last object into the current record
                 total_capacity = data_reduction = volume_space = pcnt_used = fs_space = object_space = None
                 shared_space = snapshot_space = system_space = array_load = effective_space = None
 
+                
+
+
                 if hasattr(response, 'items'):
+                    # I think items is a generator and will 
+                    # return all the items as you request lazily, doing a list()
+                    # forces it to pull in all items into an easy python construct
                     metrics_items = list(response.items)
+
+                    # we requested several metrics
                     for metric_item in metrics_items:
                         if metric_item.data:
+                            # Each metric has multiple data points, 1 each day for 7 days.
+                            # they are  from older to newer, so we will keep the last value
+                            # we find.
                             for metric_data in metric_item.data:
                                 metric_name = metric_item.name
                                 if metric_name == 'array_total_capacity':
@@ -82,8 +150,6 @@ def generate_fleet_report(pure1_api_id, pure1_pk_file, pure1_pk_pwd):
                                     shared_space = round(metric_data[1] / BYTES_IN_A_TERABYTE, 2)
                                 elif metric_name == 'array_snapshot_space':
                                     snapshot_space = round(metric_data[1] / BYTES_IN_A_TERABYTE, 2)
-                                elif metric_name == 'array_effective_used_space':
-                                    effective_space = round(metric_data[1] / BYTES_IN_A_TERABYTE, 2)
                                 elif metric_name == 'array_file_system_space':
                                     fs_space = round(metric_data[1] / BYTES_IN_A_TERABYTE, 2)
                                 elif metric_name == 'array_object_store_space':
@@ -94,24 +160,36 @@ def generate_fleet_report(pure1_api_id, pure1_pk_file, pure1_pk_pwd):
                                     array_load = round(metric_data[1], 2)
                     
                     if 'FA' in array.os:
-                        if effective_space:
-                            volume_capacity = effective_space
-                        else:
-                            try:
-                                pcnt_used = round(100 * (volume_space + shared_space +
-                                        snapshot_space + system_space) / total_capacity, 2)
-                            except:
-                                pass
-                        filewriter_fa.writerow([array.name, array.id, array.model, os_version, total_capacity, data_reduction,
+                        try:
+                            # If for some reason it didn't return data, the value will
+                            # be null and this will be a TypeError
+                            # but we still want to capture the record even if there is a 
+                            # bad value.
+                            pcnt_used = round(100 * (volume_space + shared_space +
+                                    snapshot_space + system_space) / total_capacity, 2)
+                        except TypeError as e:
+                            pass
+                        # Write entry into CSV file.
+                        filewriter_fa.writerow([array.name, array.id, array.model, array.version, total_capacity, data_reduction,
                                             pcnt_used, shared_space, volume_space, snapshot_space, system_space, array_load])
                     
                     elif 'FB' in array.os:
                         try:
                             pcnt_used = round(100 * (fs_space + object_space) / total_capacity, 2)
-                        except:
+                        except TypeError as e:
                             pass
-                        filewriter_fb.writerow([array.name, array.id, array.model, os_version, total_capacity, data_reduction,
+                        filewriter_fb.writerow([array.name, array.id, array.model, array.version, total_capacity, data_reduction,
                                             pcnt_used, fs_space, object_space])
+                else:
+                    if response.status_code == 429 or response.status_code == 404:
+                        print(response.errors[0].message)
+                        if response.errors[0].context is not None:
+                            print(response.errors[0].context)
+                        if response.status_code == 429:
+                            print("Remaining requests: " + response.headers.x_ratelimit_limit_minute) 
+                    else:     
+                        print(str.format("error code: {}\n error: {}", response.status_code, response.errors[0].message))
+                        print(str.format(" metrics: {}", str(metrics_names)))
 
 
 
@@ -129,3 +207,7 @@ if __name__ == '__main__':
 
     print("Generating Pure1 custom report")
     generate_fleet_report(ARGS.pure1_api_id, ARGS.pure1_pk_file, ARGS.password)
+    print("")
+    print('Finished')
+    
+
